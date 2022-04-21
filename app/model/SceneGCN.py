4eda95e05518d9803a2979cd84bed6f131a918c8
Syed Zohair Abbas Hadi <zohairabbashadi@hotmail.com>
2022-04-21 10:22:20 +0500

    add models

---
+++ app/model/SceneGCN.py
@@ -0,0 +1,62 @@
+# Contruct a two-layer GNN model
+import dgl.nn as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        self.conv1 = dglnn.SAGEConv(
+            in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(
+            in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = F.relu(h)
+        h = self.conv2(graph, h)
+        return h
+
+
+class MLPPredictor(nn.Module):
+    def __init__(self, in_features, out_classes):
+        super().__init__()
+        self.W = nn.Linear(in_features * 2, out_classes)
+
+    def apply_edges(self, edges):
+        h_u = edges.src['h']
+        h_v = edges.dst['h']
+        score = self.W(torch.cat([h_u, h_v], 1))
+        return {'score': score}
+
+    def forward(self, graph, h):
+        # h contains the node representations computed from the GNN defined
+        # in the node classification section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(self.apply_edges)
+            return graph.edata['score']
+
+
+class DirectionModel(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = MLPPredictor(out_features, 6)
+        
+    def forward(self, g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h)
+
+class DistanceModel(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = MLPPredictor(out_features, 3)
+        
+    def forward(self, g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h)
+
+
